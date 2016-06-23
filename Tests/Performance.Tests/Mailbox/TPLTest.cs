551aea0b78419ee463b2beead13239d97e01fb58
Pantazis Deligiannis <pdeligia@me.com>
2016-06-23 11:28:55 +0530

    added TPL dataflow mailbox benchmark

---
+++ Tests/Performance.Tests/Mailbox/TPLTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+using Microsoft.PSharp;
+using Microsoft.PSharp.Utilities;
+
+namespace Mailbox
+{
+    public class TPLTest
+    {
+        Task[] Tasks;
+        static BufferBlock<Event> Inbox = new BufferBlock<Event>();
+
+        public void Start(int numOfTasks, int numOfSendsPerTask)
+        {
+            this.Tasks = new Task[numOfTasks + 1];
+
+            for (int idx = 0; idx < numOfTasks; idx++)
+            {
+                var producer = new Task(async () =>
+                {
+                    for (int i = 0; i < numOfSendsPerTask; i++)
+                    {
+                        await Inbox.SendAsync(new Mailbox.MailEvent());
+                    }
+                });
+
+                this.Tasks[idx] = producer;
+            }
+
+            var consumer = new Task(async () =>
+            {
+                while (await Inbox.OutputAvailableAsync())
+                {
+                    await Inbox.ReceiveAsync();
+                }
+            });
+
+            this.Tasks[numOfTasks] = consumer;
+
+            Profiler profiler = new Profiler();
+            profiler.StartMeasuringExecutionTime();
+
+            foreach (var task in this.Tasks)
+            {
+                task.Start();
+            }
+
+            Task.WaitAll(Tasks);
+
+            profiler.StopMeasuringExecutionTime();
+            Console.WriteLine("... TPL data-flow sent and handled " +
+                $"{numOfTasks * numOfSendsPerTask} events in " +
+                $"'{profiler.Results()}' seconds.");
+        }
+    }
+}
--- Tests/Performance.Tests/Mailbox/Test.cs
+++ Tests/Performance.Tests/Mailbox/Test.cs
@@ -7,20 +7,22 @@ namespace Mailbox
 {
     public class Test
     {
-        static readonly int NumOfNodes = 5;
-        static readonly int NumberOfSendsPerNode = 100000;
+        static readonly int NumOfNodes = 10;
+        static readonly int NumberOfSendsPerNode = 10000;
 
         static public Profiler Profiler = new Profiler();
 
         static void Main(string[] args)
         {
+            new TPLTest().Start(NumOfNodes, NumberOfSendsPerNode);
+
             var runtime = PSharpRuntime.Create();
             Execute(runtime);
             runtime.Wait();
 
             Profiler.StopMeasuringExecutionTime();
-            Console.WriteLine($"... P# sent {NumOfNodes * NumberOfSendsPerNode} " +
-                $"events for '{Profiler.Results()}' seconds.");
+            Console.WriteLine($"... P# sent and handled {NumOfNodes * NumberOfSendsPerNode} " +
+                $"events in '{Profiler.Results()}' seconds.");
         }
 
         [Microsoft.PSharp.Test]
