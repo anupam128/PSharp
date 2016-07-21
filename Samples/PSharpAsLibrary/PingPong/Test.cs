f108d76681eff67c36282f7bfc25d4b400cefca5
akashlal <akashl@microsoft.com>
2016-07-21 16:01:23 +0530

    Testing how to enable VS code coverage.

--- Samples/PSharpAsLibrary/PingPong/Test.cs
+++ Samples/PSharpAsLibrary/PingPong/Test.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.PSharp;
+using Microsoft.PSharp.Utilities;
+using Microsoft.PSharp.TestingServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PingPong
 {
+    [TestClass]
     public class Test
     {
         static void Main(string[] args)
@@ -17,6 +21,19 @@ namespace PingPong
         public static void Execute(PSharpRuntime runtime)
         {
             runtime.CreateMachine(typeof(Server), "TheUltimateServerMachine");
+        }
+
+        [TestMethod]
+        public void TestReceivingExternalEvents()
+        {
+            var configuration = Configuration.Create();
+            configuration.Verbose = 2;
+
+            var engine = TestingEngineFactory.CreateBugFindingEngine(
+                configuration, Execute);
+            engine.Run();
+
+            Assert.AreEqual(0, engine.NumOfFoundBugs);
         }
     }
 }
