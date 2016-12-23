ff0b94f3315864d7faa7dbeaf501cb78d5631c4e
Rashmi <t-rasmud@microsoft.com>
2016-12-23 16:26:08 +0530

    testing scripts

---
+++ scripts/compileAll.bat
@@ -0,0 +1,4 @@
+.\PSharpCompiler.exe /s:D:\PSharp\Samples\Experimental\LivenessBenchmarks\BuggyBenchmarks\BuggyBenchmarks.sln /t:test
+.\PSharpCompiler.exe /s:D:\PSharp\Samples\Experimental\LivenessBenchmarks\SpinBenchmarks\SpinBenchmarks.sln /t:test
+.\PSharpCompiler.exe /s:D:\vnext\PyraStor\PyraStor\PyraStor.sln /p:NameNode.PSharp /t:test
+.\PSharpCompiler.exe /s:D:\BatchPoolServer\PoolServer\PoolServer.sln /t:test
