d527e942f6084eaecd447b2381f5ae1e0cdcfbec
Rashmi <t-rasmud@microsoft.com>
2016-12-23 15:52:24 +0530

    Raft election timer

--- Samples/Experimental/LivenessBenchmarks/BuggyBenchmarks/Raft/Timers/ElectionTimer.cs
+++ Samples/Experimental/LivenessBenchmarks/BuggyBenchmarks/Raft/Timers/ElectionTimer.cs
@@ -1,4 +1,71 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using Microsoft.PSharp;
+
+//namespace Raft
+//{
+//    internal class ElectionTimer : Machine
+//    {
+//        internal class ConfigureEvent : Event
+//        {
+//            public MachineId Target;
+
+//            public ConfigureEvent(MachineId id)
+//                : base()
+//            {
+//                this.Target = id;
+//            }
+//        }
+
+//        internal class StartTimer : Event { }
+//        internal class CancelTimer : Event { }
+//        internal class Timeout : Event { }
+
+//        private class TickEvent : Event { }
+
+//        MachineId Target;
+
+//        [Start]
+//        [OnEventDoAction(typeof(ConfigureEvent), nameof(Configure))]
+//        [OnEventGotoState(typeof(StartTimer), typeof(Active))]
+//        class Init : MachineState { }
+
+//        void Configure()
+//        {
+//            this.Target = (this.ReceivedEvent as ConfigureEvent).Target;
+//            this.Raise(new StartTimer());
+//        }
+
+//        [OnEntry(nameof(ActiveOnEntry))]
+//        [OnEventDoAction(typeof(TickEvent), nameof(Tick))]
+//        [OnEventGotoState(typeof(CancelTimer), typeof(Inactive))]
+//        [IgnoreEvents(typeof(StartTimer))]
+//        class Active : MachineState { }
+
+//        void ActiveOnEntry()
+//        {
+//            this.Send(this.Id, new TickEvent());
+//        }
+
+//        void Tick()
+//        {
+//            if (this.Random())
+//            {
+//                Console.WriteLine("\n [ElectionTimer] " + this.Target + " | timed out\n");
+//                this.Send(this.Target, new Timeout(), true);
+//            }
+
+//            this.Send(this.Id, new TickEvent());
+//            //this.Raise(new CancelTimer());
+//        }
+
+//        [OnEventGotoState(typeof(StartTimer), typeof(Active))]
+//        [IgnoreEvents(typeof(CancelTimer), typeof(TickEvent))]
+//        class Inactive : MachineState { }
+//    }
+//}
+
+using System;
 using System.Collections.Generic;
 using Microsoft.PSharp;
 
@@ -33,7 +100,6 @@ namespace Raft
         void Configure()
         {
             this.Target = (this.ReceivedEvent as ConfigureEvent).Target;
-            this.Raise(new StartTimer());
         }
 
         [OnEntry(nameof(ActiveOnEntry))]
@@ -55,12 +121,18 @@ namespace Raft
                 this.Send(this.Target, new Timeout(), true);
             }
 
-            this.Send(this.Id, new TickEvent());
-            //this.Raise(new CancelTimer());
+            this.Raise(new CancelTimer());
         }
 
+        [OnEntry(nameof(InactiveOnEntry))]
         [OnEventGotoState(typeof(StartTimer), typeof(Active))]
         [IgnoreEvents(typeof(CancelTimer), typeof(TickEvent))]
         class Inactive : MachineState { }
+
+        void InactiveOnEntry()
+        {
+            Send(Id, new StartTimer());
+        }
     }
 }
+
