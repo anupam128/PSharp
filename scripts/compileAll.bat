f463b7eed7ec537d119354c19185c0290b04a8c0
Rashmi <t-rasmud@microsoft.com>
2017-01-30 10:10:06 +0530

    changes in scripts

--- scripts/compileAll.bat
+++ scripts/compileAll.bat
@@ -2,3 +2,5 @@
 .\PSharpCompiler.exe /s:D:\PSharp\Samples\Experimental\LivenessBenchmarks\SpinBenchmarks\SpinBenchmarks.sln /t:test
 .\PSharpCompiler.exe /s:D:\vnext\PyraStor\PyraStor\PyraStor.sln /p:NameNode.PSharp /t:test
 .\PSharpCompiler.exe /s:D:\BatchPoolServer\PoolServer\PoolServer.sln /t:test
+cd D:\BatchService\src\xstore\watask\PoolManager\PoolServer
+.\comptest.cmd
--- scripts/testCycle.bat
+++ scripts/testCycle.bat
@@ -3,7 +3,10 @@ set iters=%2
 .\PSharpTester.exe /test:D:\PSharp\Samples\Experimental\LivenessBenchmarks\BuggyBenchmarks\Chord\bin\Debug\Chord.dll /max-steps:%steps% /i:%iters% /cycle-replay
 .\PSharpTester.exe /test:D:\PSharp\Samples\Experimental\LivenessBenchmarks\BuggyBenchmarks\ReplicatingStorage\bin\Debug\ReplicatingStorage.dll /max-steps:%steps% /i:%iters% /cycle-replay
 .\PSharpTester.exe /test:D:\PSharp\Samples\Experimental\LivenessBenchmarks\BuggyBenchmarks\Raft\bin\Debug\Raft.dll /max-steps:%steps% /i:%iters% /cycle-replay
+.\PSharpTester.exe /test:D:\PSharp\Samples\Experimental\LivenessBenchmarks\BuggyBenchmarks\RaftBackoff\bin\Debug\RaftBackoff.dll /max-steps:%steps% /i:%iters% /cycle-replay
+.\PSharpTester.exe /test:D:\PSharp\Samples\Experimental\LivenessBenchmarks\BuggyBenchmarks\BoundedRaft\bin\Debug\BoundedRaft.dll /max-steps:%steps% /i:%iters% /cycle-replay
+.\PSharpTester.exe /test:D:\vnext\PyraStor\NameNode.PSharp\bin\Debug\NameNode.PSharp.dll /max-steps:%steps% /i:%iters% /cycle-replay
+.\PSharpTester.exe /max-steps:%steps% /test:D:\BatchService\src\xstore\watask\PoolManager\PoolServer\IntegrationTests\bin\Debug\PSIntegrationTest.dll  /i:%iters% /method:DeletePool /cycle-replay
 .\PSharpTester.exe /test:D:\PSharp\Samples\Experimental\LivenessBenchmarks\SpinBenchmarks\SpinBenchmarks\bin\Debug\ProcessScheduler.dll /max-steps:%steps% /i:%iters% /cycle-replay
 .\PSharpTester.exe /test:D:\PSharp\Samples\Experimental\LivenessBenchmarks\SpinBenchmarks\LeaderElection\bin\Debug\LeaderElection.dll /max-steps:%steps% /i:%iters% /cycle-replay
 .\PSharpTester.exe /test:D:\PSharp\Samples\Experimental\LivenessBenchmarks\SpinBenchmarks\SlidingWindowProtocol\bin\Debug\SlidingWindowProtocol.dll /max-steps:%steps% /i:%iters% /cycle-replay
-.\PSharpTester.exe /test:D:\vnext\PyraStor\NameNode.PSharp\bin\Debug\NameNode.PSharp.dll /max-steps:%steps% /i:%iters% /cycle-replay
